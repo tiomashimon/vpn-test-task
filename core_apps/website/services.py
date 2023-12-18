99a449c4c5caab17a8769bc8d1c13cdb2317786f
tiomashimon <tiomashimon@gmai.com>
2023-12-18 19:54:41 +0100

    Selenium

--- core_apps/website/services.py
+++ core_apps/website/services.py
@@ -21,3 +21,35 @@ def change_html_href(response, user_site_name, base_url):
             new_href = f'http://localhost:8000/website/{user_site_name}/{original_href}'
             a_tag['href'] = new_href
     return str(soup)
+
+
+
+# def change_html_resource_paths(response, user_site_name, base_url):
+#     soup = BeautifulSoup(response, 'html.parser')
+
+#     for link_tag in soup.find_all('link', {'rel': 'stylesheet'}):
+#         original_href = link_tag.get('href')
+#         if original_href and not original_href.startswith(('http:', 'https:')):
+#             link_tag['href'] = f'http://localhost:8000/website/{user_site_name}/{original_href}'
+
+#     for script_tag in soup.find_all('script', {'src': True}):
+#         original_src = script_tag['src']
+#         if original_src and not original_src.startswith(('http:', 'https:')):
+#             script_tag['src'] = f'http://localhost:8000/website/{user_site_name}/{original_src}'
+
+#     for img_tag in soup.find_all('img', {'src': True}):
+#         original_src = img_tag['src']
+#         if original_src and not original_src.startswith(('http:', 'https:')):
+#             img_tag['src'] = f'http://localhost:8000/website/{user_site_name}/{original_src}'
+
+#     return str(soup)
+
+
+# def load_resource(base_url, resource_path):
+#     resource_url = f'{base_url}/{resource_path}'
+#     try:
+#         response = requests.get(resource_url)
+#         response.raise_for_status() 
+#         return response.text
+#     except requests.RequestException as e:
+#         return f'Failed to load resource {resource_path}'
